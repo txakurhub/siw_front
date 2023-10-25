7974c40a7bdd50d66c13cfb31da3c89ed2ebce3f
unknown <me.leandrop@gmail.com>
2023-10-25 07:31:20 -0300

    home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,18 @@
+import logo from "../assets/siwcargo.png";
+import ProfileMenu from "../components/ProfileMenu";
+import TicketList from "../components/TicketList";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full h-[90vh] flex flex-col items-center items-center">
+      <div className="flex flex-row w-full items-center justify-between">
+        {/* LOGO Y LISTADO DE FACTURAS */}
+        <img src={logo} alt="logo" className="w-20" />
+        <ProfileMenu />
+      </div>
+      <TicketList />
+    </section>
+  );
 };
 
 export default Home;
