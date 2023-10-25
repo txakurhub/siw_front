61f0ad0c10b021f7e5d473a3895f017450509ebe
unknown <me.leandrop@gmail.com>
2023-10-25 19:13:38 -0300

    session skip landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/siwcargo.png";
 import PrimaryButton from "../components/PrimaryButton";
+import { useEffect } from "react";
 const Landing = () => {
+  const navigate = useNavigate();
+  const userData = JSON.parse(localStorage.getItem("userData"));
+
+  useEffect(() => {
+    if (userData?.email) {
+      navigate("/dash");
+    }
+  }, [navigate, userData]);
+
   return (
     <section className="w-full h-[90vh] flex flex-col justify-center items-center">
       <img src={logo} alt="siwcargo" className="w-3/12" />
       <div className="mt-5 flex flex-col ">
         <PrimaryButton to="/login" text="Ingresá" />
         <div className="flex items-center gap-5 flex-row w-full">
-          <span className="rounded-sm border-[0.1px] border-blue-300 w-full" /> o
+          <span className="rounded-sm border-[0.1px] border-blue-300 w-full" />{" "}
+          o
           <span className=" rounded-sm border-[1px] border-blue-300 w-full" />
         </div>
         <PrimaryButton to="/signup" text="Registrate" primary={false} />
