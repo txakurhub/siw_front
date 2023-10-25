5c1e9021f59150efc402166f46ac886c2de3a99f
unknown <me.leandrop@gmail.com>
2023-10-25 07:31:02 -0300

    rf primarybtn

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -4,8 +4,8 @@ const PrimaryButton = ({ to, text, primary = true }) => {
       href={to}
       className={`px-5 py-2 m-3 font-semibold rounded-lg duration-300 ${
         primary
-          ? "bg-blue-300 hover:bg-blue-100 "
-          : "bg-blue-100 hover:bg-blue-300 "
+          ? "bg-blue-300 hover:bg-blue-100 border-2 border-blue-100"
+          : "bg-blue-100 hover:bg-blue-300 border-2 border-blue-300"
       }`}
     >
       {text}
