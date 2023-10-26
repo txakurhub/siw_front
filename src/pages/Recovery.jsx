8f2b2b61645183bf434e577cf316809194f43ee3
unknown <me.leandrop@gmail.com>
2023-10-26 14:57:39 -0300

    recovery

---
+++ src/pages/Recovery.jsx
@@ -0,0 +1,11 @@
+import PutYourEmail from "../components/PutYourEmail";
+
+const Recovery = () => {
+  return (
+    <section className="flex items-center justify-center">
+      <PutYourEmail />
+    </section>
+  );
+};
+
+export default Recovery;
