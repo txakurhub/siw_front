18dd23d4c3cf86409eb4926be0779a7f1efdae2d
unknown <me.leandrop@gmail.com>
2023-10-29 15:40:48 -0300

    add footer

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,21 @@
 import logo from "../assets/siwcargo.png";
+import Footer from "../components/Footer";
 import ProfileMenu from "../components/ProfileMenu";
 import TicketList from "../components/TicketList";
 
 const Home = () => {
   return (
-    <section className="w-full h-[90vh] flex flex-col items-center items-center">
-      <div className="flex flex-row w-full items-center justify-between">
-        {/* LOGO Y LISTADO DE FACTURAS */}
-        <img src={logo} alt="logo" className="w-20" />
-        <ProfileMenu />
-      </div>
-      <TicketList />
-    </section>
+    <>
+      <section className="w-full h-[90vh] flex flex-col items-center items-center">
+        <div className="flex flex-row w-full items-center justify-between">
+          {/* LOGO Y LISTADO DE FACTURAS */}
+          <img src={logo} alt="logo" className="w-20" />
+          <ProfileMenu />
+        </div>
+        <TicketList />
+      </section>
+      <Footer />
+    </>
   );
 };
 
